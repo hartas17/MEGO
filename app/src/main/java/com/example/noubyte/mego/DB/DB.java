a2beaae51e8c0a97178c9083d88b9c5a4965c557
hartas17 <hartas17@gmail.com>
2017-03-30 23:17:29 -0600

    empezando segunda version

---
+++ app/src/main/java/com/example/noubyte/mego/DB/DB.java
@@ -0,0 +1,8 @@
+package com.example.noubyte.mego.DB;
+
+/**
+ * Created by Noubyte on 28/03/2017.
+ */
+
+public class DB {
+}
---
+++ app/src/main/java/com/example/noubyte/mego/Models/Graphics.java
@@ -0,0 +1,7 @@
+package com.example.noubyte.mego.Models;
+
+/**
+ * Created by Noubyte on 28/03/2017.
+ */
+public class Graphics {
+}
